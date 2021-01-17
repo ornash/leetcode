d96b81a82a1069841b72981530c1fe5d89116515
ornash <41027459+ornash@users.noreply.github.com>
2021-01-17 18:48:19 -0500

    problem statements

--- src/main/scala/leetcode/java/AlienDictionary.java
+++ src/main/scala/leetcode/java/AlienDictionary.java
@@ -8,6 +8,34 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * There is a new alien language that uses the English alphabet. However, the order among letters are unknown to you.
+ *
+ * You are given a list of strings words from the dictionary, where words are sorted lexicographically by the rules of this new language.
+ *
+ * Derive the order of letters in this language, and return it. If the given input is invalid, return "". If there are multiple valid solutions, return any of them.
+ *
+ *
+ *
+ * Example 1:
+ * Input: words = ["wrt","wrf","er","ett","rftt"]
+ * Output: "wertf"
+ *
+ * Example 2:
+ * Input: words = ["z","x"]
+ * Output: "zx"
+ *
+ * Example 3:
+ * Input: words = ["z","x","z"]
+ * Output: ""
+ * Explanation: The order is invalid, so return "".
+ *
+ *
+ * Constraints:
+ * 1 <= words.length <= 100
+ * 1 <= words[i].length <= 100
+ * words[i] consists of only lowercase English letters.
+ */
 class AlienDictionary {
 
     public static void main(String[] args) {
